b93fe2dcbbf0078312cb5d4d553c220391b3ed63
dsantosferreira <unidsferreira2003@gmail.com>
2022-12-29 22:59:23 +0000

    Added airline class

---
+++ src/Airline.cpp
@@ -0,0 +1,8 @@
+#include "Airline.h"
+
+Airline::Airline(string code, string name, string callSign, string country) {
+    this->code = code;
+    this->name = name;
+    this->callSign = callSign;
+    this->country = country;
+}
---
+++ src/Airline.h
@@ -0,0 +1,16 @@
+#ifndef AERIALTRANSPORTATION_AIRLINE_H
+#define AERIALTRANSPORTATION_AIRLINE_H
+
+#include <string>
+
+using namespace std;
+
+class Airline {
+private:
+    string code, name, callSign, country;
+
+public:
+    Airline(string code, string name, string callSign, string country);
+};
+
+#endif
