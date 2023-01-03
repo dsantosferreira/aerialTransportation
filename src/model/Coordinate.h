c08432ce813394055b0d776445d7e342b701d300
dsantosferreira <unidsferreira2003@gmail.com>
2023-01-03 02:02:18 +0000

    Hotfix

--- src/model/Coordinate.h
+++ src/model/Coordinate.h
@@ -13,9 +13,4 @@ public:
     float distance(Coordinate coordinate1) const;
 };
 
-<<<<<<< HEAD:src/Coordinate.h
-=======
-
-
->>>>>>> eba6b3e083d0f0d1abaf52c24cb7f810f325eaae:src/model/Coordinate.h
 #endif //AERIALTRANSPORTATION_COORDINATE_H
