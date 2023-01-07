0d521e498fb3d4549b38635860268b4b29693a3d
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2023-01-07 20:22:47 +0000

    Adding decumentation to the new classes

--- src/menu/show/ShowAirline.cpp
+++ src/menu/show/ShowAirline.cpp
@@ -3,10 +3,17 @@
 //
 
 #include "ShowAirline.h"
-
+/**Constructor of the class ShowAirline  a class that extends MenuItem
+ * @brief Constructor of the class ShowAirline
+ * @param currMenuPage the current page of the menu
+ * @param database the database containing all the information
+ */
 ShowAirline::ShowAirline(int &currMenuPage, Database &database): MenuItem(currMenuPage,database) {
 }
-
+/**Asks the user for an airline code or name and displays its code and name
+ * @brief Search for an airline and displays its code and name
+ * complexity: O(1) when its given the airline code O(N) when its given its name being N the number of airlines
+ */
 void ShowAirline::execute() {
     system("clear");
     cout << "\033[32m Enter the code or the name of the airline you want to find: ";
@@ -20,12 +27,19 @@ void ShowAirline::execute() {
         if (database.getAirlines().find(Airline(s)) != database.getAirlines().end()) {
             cout << "\033[0m _______________________________________________________________________________"
                  << endl;
+            cout << "|\033[40m                                    Airline                                    \033[0m|"
+                 << endl;
+            cout << "|\033[40m_______________________________________________________________________________\033[0m|"
+                 << endl;
             cout << "|\033[100m ";
             Airline airline = database.getAirline(s);
             airline.print();
             cout << "\033[0m|" << endl
-                 << "|\033[100m_______________________________________________________________________________\033[0m|"
+             << "|\033[47m"
+                 << "\033[30m"
+                 << "_______________________________________________________________________________\033[0m|"
                  << endl;
+            cout << "|\033[40m_______________________________________________________________________________\033[0m|";
             cout << "\033[0m" << endl;
             cout << endl << "\033[32mEnter anything to go back: ";
             cin >> s;
@@ -36,11 +50,18 @@ void ShowAirline::execute() {
         if (airline.getName() == s) {
             cout << "\033[0m _______________________________________________________________________________"
                  << endl;
+            cout << "|\033[40m                                    Airline                                    \033[0m|"
+                 << endl;
+            cout << "|\033[40m_______________________________________________________________________________\033[0m|"
+                 << endl;
             cout << "|\033[100m ";
             airline.print();
             cout << "\033[0m|" << endl
-                 << "|\033[100m_______________________________________________________________________________\033[0m|"
+                 << "|\033[47m"
+                 << "\033[30m"
+                 << "_______________________________________________________________________________\033[0m|"
                  << endl;
+            cout << "|\033[40m_______________________________________________________________________________\033[0m|";
             cout << "\033[0m" << endl;
             cout << endl << "\033[32mEnter anything to go back: ";
             cin >> s;
--- src/menu/show/ShowAirport.cpp
+++ src/menu/show/ShowAirport.cpp
@@ -3,10 +3,17 @@
 //
 
 #include "ShowAirport.h"
-
-ShowAirport::ShowAirport(int &currMenuPage, Database &database1) : MenuItem(currMenuPage, database1) {
+/**Constructor of the class ShowAirport  a class that extends MenuItem
+ * @brief Constructor of the class ShowAirport
+ * @param currMenuPage the current page of the menu
+ * @param database the database containing all the information
+ */
+ShowAirport::ShowAirport(int &currMenuPage, Database &database) : MenuItem(currMenuPage, database) {
 }
-
+/**Asks the user for an airport code or name and displays its code/name/country/ city
+ * @brief Search for an airport and displays its code/name/country/ city
+ * complexity: O(1) when its given the airport code O(N) when its given its name being N the number of airports
+ */
 void ShowAirport::execute() {
     system("clear");
     cout << "\033[32m Enter the code or the name  of the airport you want to find: ";
@@ -20,6 +27,9 @@ void ShowAirport::execute() {
         if (database.getAirports().find(Airport(s)) != database.getAirports().end()) {
             cout << "\033[0m _________________________________________________________________________________________"
                  << endl;
+            cout << "|\033[40m                                         Airport                                         \033[0m|"<<endl;
+            cout << "|\033[40m_________________________________________________________________________________________\033[0m|"<<endl;
+
             cout << "|\033[100m ";
             Airport airport = database.getAirport(s);
             cout << airport.getCode() << "| " << airport.getName() << "| " << airport.getCountry() << "-"
@@ -27,8 +37,10 @@ void ShowAirport::execute() {
             for (int i = 0;
                  i < 80 - airport.getName().length() - airport.getCity().length() - airport.getCountry().length(); i++)
                 cout << " ";
-            cout << "\033[0m|" << endl
-                 << "|\033[100m_________________________________________________________________________________________\033[0m|"
+            cout << "\033[0m|" << endl;
+            cout << "|\033[47m\033[30m_________________________________________________________________________________________\033[0m|"
+                 << endl;
+                cout << "|\033[40m_________________________________________________________________________________________\033[0m|"
                  << endl;
             cout << "\033[0m" << endl;
             cout << endl << "\033[32mEnter anything to go back: ";
@@ -40,15 +52,19 @@ void ShowAirport::execute() {
             if (airport.getName() == s) {
                 cout << "\033[0m _________________________________________________________________________________________"
                      << endl;
-                cout << "|\033[100m ";
+                cout << "|\033[40m                                         Airport                                         \033[0m|"<<endl;
+                cout << "|\033[40m_________________________________________________________________________________________\033[0m|"<<endl;
 
+                cout << "|\033[100m ";
                 cout << airport.getCode() << "| " << airport.getName() << "| " << airport.getCountry() << "-"
                      << airport.getCity();
                 for (int i = 0;
                      i < 80 - airport.getName().length() - airport.getCity().length() - airport.getCountry().length(); i++)
                     cout << " ";
-                cout << "\033[0m|" << endl
-                     << "|\033[100m_________________________________________________________________________________________\033[0m|"
+                cout << "\033[0m|" << endl;
+                cout << "|\033[47m\033[30m_________________________________________________________________________________________\033[0m|"
+                     << endl;
+                cout << "|\033[40m_________________________________________________________________________________________\033[0m|"
                      << endl;
                 cout << "\033[0m" << endl;
                 cout << endl << "\033[32mEnter anything to go back: ";
